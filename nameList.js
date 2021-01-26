80238c66692b91354e146636c76f717f2fde4bd3
Shaffan <spacey@pop-os.localdomain>
2021-01-26 18:35:50 -0500

    fixed for loop that added JS framework names to an array from an object

--- nameList.js
+++ nameList.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 
 const names = JSON.parse(fs.readFileSync('names.json'))
 let nameList = []
+
 for (let i = 0; i < names.length; i++) {
     nameList.push(names[i].name)
 }
